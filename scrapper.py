2ccd3830900ae52441e54f3377ddd62061810d08
ozoubia <ozoubia@gmail.com>
2020-08-26 23:20:59 +0100

    working on the html scrapping

--- scrapper.py
+++ scrapper.py
@@ -1,2 +1,18 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+
+my_url = "https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics+card"
+
+# opening the connection 
+uClient = uReq(my_url)
+page_html = uClient.read()
+# closing the connection 
+uClient.close()
+
+# paring the html 
+page_soup = soup(page_html, "html.parser")
+
+# grabs each products 
+items = page_soup.findAll("div", {"class": "item-container"})
+
+print(items[0].div)
